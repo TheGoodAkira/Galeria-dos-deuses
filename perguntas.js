dc2c4b574f3494a2cb71335b3aad2b00548f26bb
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2025-01-18 18:21:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+criaCartao(
+    'Literatura',
+    'Os Sete Maridos De Evelyn Hugo',
+    '"Os Sete Maridos de Evelyn Hugo", de Taylor Jenkins Reid, narra a vida da icônica atriz de Hollywood, Evelyn Hugo, que revela à jornalista Monique Grant os altos e baixos de seus sete casamentos, explorando temas de identidade, amor e autenticidade em um mundo dominado por preconceitos.'
+)
+
+criaCartao(
+    'Cinema',
+    'A Culpa é das Estrelas',
+    '"A Culpa é das Estrelas", baseado no romance de John Green, é um tocante filme que segue Hazel Grace Lancaster, uma adolescente com câncer que, ao conhecer Augustus Waters em um grupo de apoio, desenvolve uma intensa relação enquanto juntos enfrentam os desafios da doença e exploram temas de amor, perda e a busca por significado na vida.'
+)
+
+criaCartao(
+    'Literatura',
+    'Eu e Esse Meu Coração',
+    '"Eu e Esse Meu Coração", de Carina Rissi, é um romance contemporâneo que acompanha a jovem Sofia em seu dilema entre seguir seu coração ou atender às expectativas da sociedade, enquanto enfrenta desafios amorosos com o enigmático Miguel e explora temas de autodescoberta e a busca pela felicidade.'
+)
+
+criaCartao(
+    'Cinema',
+    'Nimona',
+    '"Nimona" é uma animação vibrante baseada na webcomic de ND Stevenson, que segue a jovem shapeshifter Nimona em sua aliança com o cavaleiro Ballister Blackheart, injustamente acusado de um crime, enquanto juntos desafiam a ordem em um reino futurista que mescla elementos medievais e tecnologia, explorando temas de amizade, aceitação e a complexidade de ser um vilão ou herói.'
+)
+
+criaCartao(
+    'Literatura',
+    'Verity',
+    '"Verity", de Colleen Hoover, é um thriller psicológico que segue Lowen Ashleigh, uma escritora em dificuldades contratada para finalizar a série de livros de Verity Crawford, uma autora famosa incapacitada, e que, ao investigar seus arquivos, descobre um manuscrito revelador que expõe verdades perturbadoras sobre a vida de Verity, levando Lowen a questionar a veracidade das palavras e as motivações por trás delas em um jogo psicológico cheio de reviravoltas.'
+)
+
+criaCartao(
+    'Cinema',
+    'Robô Selvagem',
+    '"Robô Selvagem" é uma animação divertida que narra a jornada de um robô desajeitado que se torna um herói inesperado ao se unir a um grupo de crianças para salvar sua cidade, explorando temas de amizade, coragem e aceitação das diferenças, enquanto oferece visuais vibrantes e diálogos espirituosos que cativam tanto jovens quanto adultos.'
+)
+criaCartao(
+    'Literatura',
+    'Frankenstein',
+    '"Frankenstein", de Mary Shelley, é um marco da literatura gótica que relata a trágica história de Victor Frankenstein, um cientista que, em sua busca por conhecimento, cria uma criatura a partir de cadáveres e, ao abandoná-la, desencadeia eventos devastadores que exploram a natureza da humanidade, a busca por aceitação e as consequências da ambição desmedida, enquanto a criatura, rejeitada, luta por amor e compreensão, levantando questões sobre ética na ciência e a responsabilidade da criação.'
+)
