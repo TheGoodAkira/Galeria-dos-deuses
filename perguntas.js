7b77915d7c9e2d4e10fa6753e130f3a9bce40a07
The Good Akira <168312957+TheGoodAkira@users.noreply.github.com>
2025-03-01 16:36:40 -0300

    atu

--- perguntas.js
+++ perguntas.js
@@ -1,40 +1,46 @@
 criaCartao(
-    'Literatura',
-    'Os Sete Maridos De Evelyn Hugo',
-    '"Os Sete Maridos de Evelyn Hugo", de Taylor Jenkins Reid, narra a vida da icônica atriz de Hollywood, Evelyn Hugo, que revela à jornalista Monique Grant os altos e baixos de seus sete casamentos, explorando temas de identidade, amor e autenticidade em um mundo dominado por preconceitos.'
+    'O que é?',
+    'Mitologia Grega',
+    'A mitologia grega é um conjunto de histórias e crenças que explicam a origem do mundo, a natureza dos deuses e heróis, e eventos históricos, refletindo as tradições e valores da antiga Grécia.'
 )
 
 criaCartao(
-    'Cinema',
-    'A Culpa é das Estrelas',
+    'O que é?',
+    'Mitologia Nórdica',
     '"A Culpa é das Estrelas", baseado no romance de John Green, é um tocante filme que segue Hazel Grace Lancaster, uma adolescente com câncer que, ao conhecer Augustus Waters em um grupo de apoio, desenvolve uma intensa relação enquanto juntos enfrentam os desafios da doença e exploram temas de amor, perda e a busca por significado na vida.'
 )
 
 criaCartao(
-    'Literatura',
-    'Eu e Esse Meu Coração',
+    'O que é?',
+    'Mitologia Egípcia',
     '"Eu e Esse Meu Coração", de Carina Rissi, é um romance contemporâneo que acompanha a jovem Sofia em seu dilema entre seguir seu coração ou atender às expectativas da sociedade, enquanto enfrenta desafios amorosos com o enigmático Miguel e explora temas de autodescoberta e a busca pela felicidade.'
 )
 
 criaCartao(
-    'Cinema',
-    'Nimona',
+    'O que é?',
+    'Mitologia Celta',
     '"Nimona" é uma animação vibrante baseada na webcomic de ND Stevenson, que segue a jovem shapeshifter Nimona em sua aliança com o cavaleiro Ballister Blackheart, injustamente acusado de um crime, enquanto juntos desafiam a ordem em um reino futurista que mescla elementos medievais e tecnologia, explorando temas de amizade, aceitação e a complexidade de ser um vilão ou herói.'
 )
 
 criaCartao(
-    'Literatura',
-    'Verity',
+    'O que é?',
+    'Mitologia Hindu',
     '"Verity", de Colleen Hoover, é um thriller psicológico que segue Lowen Ashleigh, uma escritora em dificuldades contratada para finalizar a série de livros de Verity Crawford, uma autora famosa incapacitada, e que, ao investigar seus arquivos, descobre um manuscrito revelador que expõe verdades perturbadoras sobre a vida de Verity, levando Lowen a questionar a veracidade das palavras e as motivações por trás delas em um jogo psicológico cheio de reviravoltas.'
 )
 
 criaCartao(
-    'Cinema',
-    'Robô Selvagem',
+    'O que é?',
+    'Mitologia Africana',
     '"Robô Selvagem" é uma animação divertida que narra a jornada de um robô desajeitado que se torna um herói inesperado ao se unir a um grupo de crianças para salvar sua cidade, explorando temas de amizade, coragem e aceitação das diferenças, enquanto oferece visuais vibrantes e diálogos espirituosos que cativam tanto jovens quanto adultos.'
 )
 criaCartao(
-    'Literatura',
-    'Frankenstein',
+    'O que é?',
+    'Mitologia Japonesa',
+    '"Frankenstein", de Mary Shelley, é um marco da literatura gótica que relata a trágica história de Victor Frankenstein, um cientista que, em sua busca por conhecimento, cria uma criatura a partir de cadáveres e, ao abandoná-la, desencadeia eventos devastadores que exploram a natureza da humanidade, a busca por aceitação e as consequências da ambição desmedida, enquanto a criatura, rejeitada, luta por amor e compreensão, levantando questões sobre ética na ciência e a responsabilidade da criação.'
+)
+
+criaCartao(
+    'O que é?',
+    'Mitologia Chinesa',
     '"Frankenstein", de Mary Shelley, é um marco da literatura gótica que relata a trágica história de Victor Frankenstein, um cientista que, em sua busca por conhecimento, cria uma criatura a partir de cadáveres e, ao abandoná-la, desencadeia eventos devastadores que exploram a natureza da humanidade, a busca por aceitação e as consequências da ambição desmedida, enquanto a criatura, rejeitada, luta por amor e compreensão, levantando questões sobre ética na ciência e a responsabilidade da criação.'
 )
